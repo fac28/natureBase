d32026c9560bf9f69687af39654f70fdd7258a1a
sgroil <laurencesgroi@hotmail.co.uk>
2023-09-26 17:40:54 +0100

    added error throw

--- src/model/getPosts.js
+++ src/model/getPosts.js
@@ -10,7 +10,7 @@ const getPosts = () => {
 
   try {
     return db.prepare(select_posts).all();
-  } catch {
+  } catch (error) {
     console.error("Error fetching posts:", error.message);
     throw error;
   }
