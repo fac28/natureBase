db7dcb34c0029f47c93f8e713fae1ac0abbb7d7a
GeorgeKlemperer <george@klemperer.co.uk>
2023-09-26 16:37:24 +0100

    changed back to 3000

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const server = require("./server.js");
 
 // Leave the port to 3000 - fly.io default standard
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
