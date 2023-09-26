19269ba123dadf57df57bd14b79f6a5816454b44
sgroil <laurencesgroi@hotmail.co.uk>
2023-09-26 11:20:05 +0100

    initial commit
    Closes #11

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const app= express();
+const staticHandler = express.static("public")
 
-const server = express();
+//const model = require("./model/getPost");
+const Layout = require('./templates')
 
+//Middleware
+app.use(staticHandler);
 
+//Routes
+app.get('/', (req, res) => {
+  res.send(Layout())
+})
 
+module.exports = app;
