e3c3a644887cb55eb084af29d92086c664795a59
nichgalzin <nichgalzin@gmail.com>
2023-09-27 15:40:38 +0100

    Added like route and module
    Closes #38

--- src/server.js
+++ src/server.js
@@ -1,27 +1,29 @@
 const express = require("express");
 const app = express();
 const staticHandler = express.static("public");
-const getPosts = require("./model/getPosts.js");
+const bodyParser = require('body-parser');
 
+const getPosts = require("./model/getPosts.js");
 const searchRoutes = require("./routes/search.js");
 const addRoutes = require("./routes/add.js");
+const likeRoutes = require('./routes/like.js');
 
 //const model = require("./model/getPost");
 const home = require("./templates");
 const templates = require("./templates");
 
 //Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(staticHandler);
 app.use("/search", searchRoutes);
 app.use("/add", addRoutes);
+app.use("/like", likeRoutes);
 
 //Routes
 app.get("/", (req, res) => {
-  console.log(getPosts());
   const html = templates.home();
 
   res.send(html);
 });
-
 
 module.exports = app;
