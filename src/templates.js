79e87ad3a740dc6f4cd81759104752d1844d28d1
sgroil <laurencesgroi@hotmail.co.uk>
2023-09-27 15:34:39 +0100

    more styling changes

--- src/templates.js
+++ src/templates.js
@@ -50,7 +50,8 @@ ${displayPosts()}
 function displayPosts() {
   return getPosts()
     .map(
-      (post) => /*html*/ `<img class="photo" src="${post.picture}"> 
+      (post) => /*html*/ `<div class="posts">
+      <img class="photo" src="${post.picture}"> 
       <div class="details">
       <p class="content">${post.content}</p>
     
@@ -60,6 +61,7 @@ function displayPosts() {
         </p>
         <p>Likes: ${post.likes}</p>
         
+      </div>
       </div>
 
       
@@ -71,7 +73,8 @@ function displayPosts() {
 
 function submissionForm(errors = {}, values = {}) {
   return /*html*/ `
-  <form action="/add" method="POST">
+  
+  <form class="centre add-form" action="/add" method="POST">
       <label for="username">Username:</label>
         <input 
           id="username"
@@ -106,6 +109,55 @@ function submissionForm(errors = {}, values = {}) {
         ${validation(errors.location)}
         <button type="submit">Submit</button>
     </form>
+    
+  `;
+}
+
+function form() {
+  return /*html*/ `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>natureBase</title>
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;500;900&display=swap" rel="stylesheet">
+  </head>
+  <body>
+  <header class="header">
+  <div class="header-content">
+  <div class="centre">
+  <div class="title-container">
+  <h1 class="title">natureBase</h1>
+  <h2 class="slogan">london's picture book</h2>
+  </div>
+  </div>
+    <nav>
+
+      <form action="/add" method="GET">
+          <button class="icon add" type="submit">
+           <img src="images/plus.svg" alt="Add Icon">
+          </button>
+      </form>
+      
+      <form action="/" method="POST">
+          <button class="icon search" type="submit">
+            <img src="images/search.svg" alt="Search Icon">
+          </button>
+      </form>
+  </nav>
+  </div>
+</header>
+${submissionForm((errors = {}), (values = {}))}
+<main>
+${displayPosts()}
+</main>
+  </body>
+  </html>
   `;
 }
 
@@ -120,4 +172,4 @@ function validation(message) {
     return "";
   }
 }
-module.exports = { home, submissionForm };
+module.exports = { home, submissionForm, form };
