472d25ee1684f691080b23bc9dcd64136efe31fe
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2023-09-27 15:52:57 +0100

    Revert "Added like route and module"

--- src/server.js
+++ src/server.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const app = express();
 const staticHandler = express.static("public");
-const bodyParser = require('body-parser');
-
 const getPosts = require("./model/getPosts.js");
+
 const searchRoutes = require("./routes/search.js");
 const addRoutes = require("./routes/add.js");
-const likeRoutes = require('./routes/like.js');
 
 //const model = require("./model/getPost");
 const home = require("./templates");
 const templates = require("./templates");
 
 //Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(staticHandler);
 app.use("/search", searchRoutes);
 app.use("/add", addRoutes);
-app.use("/like", likeRoutes);
 
 //Routes
 app.get("/", (req, res) => {
@@ -26,5 +22,6 @@ app.get("/", (req, res) => {
 
   res.send(html);
 });
+
 
 module.exports = app;
