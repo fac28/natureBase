c196e4766676fee49bf920cc0f503e78d7d84b38
sgroil <laurencesgroi@hotmail.co.uk>
2023-09-27 12:52:25 +0100

    Initial styling
    Relates #15

--- src/templates.js
+++ src/templates.js
@@ -1,4 +1,4 @@
-const getPosts = require("./model/getPosts.js")
+const getPosts = require("./model/getPosts.js");
 
 function home() {
   return /*html*/ `
@@ -10,24 +10,33 @@ function home() {
     <title>natureBase</title>
     <link rel="stylesheet" href="normalize.css">
     <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;500;900&display=swap" rel="stylesheet">
   </head>
   <body>
-  <header>
-  <h1>natureBase</h1>
-  <h2>london's picture book</h2>
+  <header class="header">
+  <div class="header-content">
+  <div class="centre">
+  <div class="title-container">
+  <h1 class="title">natureBase</h1>
+  <h2 class="slogan">london's picture book</h2>
+  </div>
+  </div>
     <nav>
       <form action="/add" method="POST">
-          <button class="add" type="submit">
+          <button class="icon add" type="submit">
             <img src="images/plus.svg" alt="Add Icon">
           </button>
       </form>
       
       <form action="/" method="POST">
-          <button class="search" type="submit">
+          <button class="icon search" type="submit">
             <img src="images/search.svg" alt="Search Icon">
           </button>
       </form>
   </nav>
+  </div>
 </header>
 <main>
 ${displayPosts()}
@@ -39,19 +48,24 @@ ${displayPosts()}
 }
 
 function displayPosts() {
- return getPosts()
-  .map(
-    (post) =>
-      `<img src="${post.picture}"> 
-      <p>${post.content}</p>
-      <p>${post.username} </p>
-      <p>${post.created_at}</p>
-      <p>Location: ${post.location}</p>
-      <p>Likes: ${post.likes}</p>
-      `
-  )
-  .reverse()
-  .join("");
+  return getPosts()
+    .map(
+      (post) => /*html*/ `<img class="photo" src="${post.picture}"> 
+      <div class="details">
+      <p class="content">${post.content}</p>
+    
+        <p class="info">
+          <span class="location">${post.location}</span>
+          <span class="right">- ${post.username} <span class="date">${post.created_at}</span></span>
+        </p>
+        <p>Likes: ${post.likes}</p>
+        
+      </div>
 
+      
+      `
+    )
+    .reverse()
+    .join("");
 }
 module.exports = { home };
