14f1e12e01598c1c805f15b93623c22a8adbbf17
sgroil <laurencesgroi@hotmail.co.uk>
2023-09-28 11:59:48 +0100

    added NOT NULL to likes column again

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS posts (
     picture TEXT,
 
     location TEXT,
-    likes INTEGER DEFAULT NOT NULL 0,
+    likes INTEGER NOT NULL DEFAULT  0,
 
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP  
 );
--- src/server.js
+++ src/server.js
@@ -1,35 +1,34 @@
-const express = require("express");
-const app = express();
-const staticHandler = express.static("public");
-const bodyParser = require("body-parser");
+const express = require('express')
+const app = express()
+const staticHandler = express.static('public')
+const bodyParser = require('body-parser')
 
-const deleteRoutes = require("./routes/delete");
-const searchRoutes = require("./routes/search.js");
-const addRoutes = require("./routes/add.js");
-const likeRoutes = require("./routes/like.js");
-const getPosts = require("./model/getPosts.js");
+const deleteRoutes = require('./routes/delete')
+const searchRoutes = require('./routes/search.js')
+const addRoutes = require('./routes/add.js')
+const likeRoutes = require('./routes/like.js')
+const getPosts = require('./model/getPosts.js')
 
 //const model = require("./model/getPost");
 
-
-const templates = require("./templates");
+const templates = require('./templates')
 
 //Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(staticHandler);
-app.use("/search", searchRoutes);
-app.use("/add", addRoutes);
-app.use("/like", likeRoutes);
-app.use("/delete", deleteRoutes);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(staticHandler)
+app.use('/search', searchRoutes)
+app.use('/add', addRoutes)
+app.use('/like', likeRoutes)
+app.use('/delete', deleteRoutes)
 
 //Routes
 
-app.get("/", (req, res) => {
-  // console.log(getPosts());
-  const content = templates.displayPosts(getPosts());
-  const html = templates.home(content);
+app.get('/', (req, res) => {
+    // console.log(getPosts());
+    const content = templates.displayPosts(getPosts())
+    const html = templates.home(content)
 
-  res.send(html);
-});
+    res.send(html)
+})
 
-module.exports = app;
+module.exports = app
