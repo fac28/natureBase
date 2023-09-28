d1ca70041c843c83fc6e567868b33934db5e11e6
nichgalzin <nichgalzin@gmail.com>
2023-09-28 11:35:06 +0100

    Error handling added to like, home and delete routes
    Relates #55

--- src/routes/delete.js
+++ src/routes/delete.js
@@ -1,15 +1,19 @@
-const express = require("express");
+const express = require('express')
 
-const deletePost = require("../model/deletePost");
+const deletePost = require('../model/deletePost')
 
 //Variables
-const router = express.Router();
-const templates = require("../templates");
+const router = express.Router()
 
-router.post("/", (req, res) => {
-  const item_id = req.body.item_id;
-  deletePost(item_id);
-  res.redirect("/");
-});
+router.post('/', (req, res) => {
+    try {
+        const item_id = req.body.item_id
+        deletePost(item_id)
+        res.redirect('/')
+    } catch (error) {
+        console.error('Error with route:', error.message)
+        throw error
+    }
+})
 
-module.exports = router;
+module.exports = router
--- src/routes/search.js
+++ src/routes/search.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 //Variables
 const router = express.Router();
-const templates = require("../templates");
 
 router.get("/", (req, res) => {
   //   res.send(templates.home());
